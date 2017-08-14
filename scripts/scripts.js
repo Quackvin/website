381a0942143ca4044ee51ce93e13fc677fd8d3c8
Kevin Kuek <kevin.kuek.95@gmail.com>
2017-08-14 19:55:52 +0800

    - add positioning effects - add extra area

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-	alert('document loaded')
+	$(window).scroll(function(){
+		if($(window).width()>550){
+			if($("#body1").offset().top < $(window).scrollTop()){
+				$("#body1").css("position","fixed");
+				$("#body1").css("top","5%");
+			}
+			else if ($(window).scrollTop() < $(window).height()/4){
+				$("#body1").css("position","absolute");
+				$("#body1").css("top","25vw");
+			}
+		}
+	})
+	$(window).on('resize',function(){
+		if($(window).width()>550){
+			if ($(window).scrollTop() < $(window).height()/4){
+				$("#body1").css("position","absolute");
+				$("#body1").css("top","25vw");
+			}
+			else{
+				$("#body1").css("position","fixed");
+				$("#body1").css("top","5%");
+			}
+		}
+		else{
+			$("#body1").css("position","absolute");
+			$("#body1").css("top","70vw");
+		}
+	});
 });
