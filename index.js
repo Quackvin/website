e5ea2a6c45a2084be5285042dacbeaa1b1287901
Kevin Kuek <kevin.kuek.95@gmail.com>
2017-10-13 13:24:58 +0800

    - change to single page site

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ var path = require('path');
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
-	res.sendFile(path.join(__dirname + '/templates/index.html'));
+	res.sendFile(path.join(__dirname + '/templates/onepage.html'));
 })
 .get('/about', function(req, res){
-	res.sendFile(path.join(__dirname + '/templates/about.html'))
+	res.sendFile(path.join(__dirname + '/templates/onepage.html#about'))
 })
 .get('/portfolio', function(req, res){
-	res.sendFile(path.join(__dirname + '/templates/portfolio.html'))
+	res.sendFile(path.join(__dirname + '/templates/onepage.html#portfolio'))
 })
 .get('/contact', function(req, res){
-	res.sendFile(path.join(__dirname + '/templates/contact.html'))
+	res.sendFile(path.join(__dirname + '/templates/onepage.html#contact'))
 });
 
 app.listen(8080);
+console.log('listening on port 8080');
